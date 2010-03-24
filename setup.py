c16dac6725fc8183c65c3cc3994aa7e2171abd51
frisi <frisi@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-24 09:42:01 +0000

    don't mix up package with GS profile version

--- setup.py
+++ setup.py
@@ -3,18 +3,12 @@ import os
 from setuptools import setup, find_packages
 from xml.dom.minidom import parse
 
-def readversion():
-    mdfile = os.path.join(os.path.dirname(__file__), 'collective', 'superfish', 
-                          'profiles', 'default', 'metadata.xml')
-    metadata = parse(mdfile)
-    assert metadata.documentElement.tagName == "metadata"
-    return metadata.getElementsByTagName("version")[0].childNodes[0].data
 
 def read(*pathnames):
     return open(os.path.join(os.path.dirname(__file__), *pathnames)).read()
 
 setup(name='collective.superfish',
-      version=readversion().strip(),
+      version='0.4-dev',
       description="A suckerfish/superfish integration into plone",
       long_description='\n'.join([
         read('docs', 'README.txt'),
