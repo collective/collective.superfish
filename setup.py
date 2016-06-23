2a0440613351c7fc128a150d6c6ccef533801383
Harald Friessnegger <harald@webmeisterei.com>
2016-06-23 12:35:56 +0200

    Preparing release 1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*pathnames):
 
 setup(
     name='collective.superfish',
-    version='1.1.dev0',
+    version='1.1',
     description="A suckerfish/superfish integration into plone",
     long_description='\n'.join([
         read('README.rst'),
