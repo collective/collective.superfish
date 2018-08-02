784b535aed34b0cebebee5c048a25828c6287894
Peter Mathis <peter.mathis@kombinat.at>
2018-08-02 10:58:47 +0200

    Back to development: 2.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*pathnames):
 
 setup(
     name='collective.superfish',
-    version='2.0',
+    version='2.1.dev0',
     description="A suckerfish/superfish integration into plone",
     long_description='\n'.join([
         read('README.rst'),
